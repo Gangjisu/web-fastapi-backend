20bd44be6ccb4d1d6e9716cda6034de9d2dac01d
Gangjisu <kjs511808@gmail.com>
2025-04-30 15:16:52 +0900

    api update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import json
 import requests
+import os
 
-API_KEY = "b3KKuHC2eR9V5hiDfAzG3aHvzWnkHx4T9zKZpFSl"
-BASE_URL = "https://open-api.bser.io"
+BSER_API_KEY  = os.getenv('BSER_API_KEY')
+BSER_BASE_URL = os.getenv('BSER_BASE_URL')
 
 app = FastAPI()
 
@@ -16,8 +18,32 @@ app.add_middleware(
 
 @app.get("/user")
 def get_user_info(nickname: str):
-    url = f"{BASE_URL}/v1/user/nickname"
-    headers = {"x-api-key": API_KEY}
+    url = f"{BSER_BASE_URL}/v1/user/nickname"
+    headers = {"x-api-key": BSER_API_KEY}
     params = {"query": nickname}
     resp = requests.get(url, headers=headers, params=params)
-    return resp.json()
+    return resp.json().get("user",{})
+
+# [2] Get user rank info
+@app.get("/user/rank")
+def get_user_rank(user_num, season_id):
+    url = f"{BSER_BASE_URL}/v1/rank/{user_num}/{season_id}/3" #{team_mode}
+    resp = requests.get(url, headers=headers) # print("▶ Get User Rank:", resp.status_code)
+    return resp.json().get("userRank",{})
+# [2]
+
+# [3] Get user stats
+@app.get("/user/stats")
+def get_user_stats(user_num, season_id):
+    url = f"{BSER_BASE_URL}/v1/user/stats/{user_num}/{season_id}"
+    resp = requests.get(url, headers=headers) # print("▶ Get User Stats:", resp.status_code)
+    return resp.json().get("userStats",{})
+# [3]
+
+# [4] Get user games
+@app.get("/user/games")
+def get_user_games(user_num):
+    url = f"{BSER_BASE_URL}/v1/user/games/{user_num}"
+    resp = requests.get(url, headers=headers) # print("▶ Get User Games:", resp.status_code)
+    return resp.json().get("userGames",{})
+# [4]
